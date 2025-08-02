75b0f841232a27c8c96c861f739ce94a18595acd
bidgeycodes <brianna.c.mcneil@gmail.com>
2025-08-02 12:57:54 -0400

    app.vue updates

--- out-of-context-tectone/src/App.vue
+++ out-of-context-tectone/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="app">
+  <div class="quote-generator">
+    <div class="header">
+      <img class="logo" src="">
+      <h1 class="title">Tectcone Out Of Context</h1>
+    </div>
+      <div class="quote-container">
+        <p class="quote-text">{{ quoteText }}</p>
+    </div>
+  <button @click="getNewQuote" class="btn-generate">Generate</button>
   </div>
-</template>
+ </template>
 
 <script setup>
 </script>
