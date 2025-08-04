836d2642847eab3cf8d70ff89c42ad357ae27110
briannalytical <briannamcneil.dev@gmail.com>
2025-08-04 16:12:53 -0400

    exported db data and create table if not exists

--- schema.sql
+++ schema.sql
@@ -1,9 +1,7 @@
-CREATE TABLE quotes (
+CREATE TABLE IF NOT EXISTS quotes (
 id SERIAL PRIMARY KEY,
 quote_text TEXT NOT NULL,
 video_name TEXT,
 date_added TIMESTAMP DEFAULT now()
 video_date DATE
 );
-
-INSERT INTO quotes (quote_text, )
