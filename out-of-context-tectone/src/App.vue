60a1ebc0483b39dd990cf26937651b486d94aa3c
bidgeycodes <brianna.c.mcneil@gmail.com>
2025-08-11 20:03:50 -0400

    created quote generator component | moved app.vue to component

--- out-of-context-tectone/src/App.vue
+++ out-of-context-tectone/src/App.vue
@@ -1,20 +1,3 @@
 <template>
-  <div class="quote-generator">
-    <div class="header">
-      <img class="logo" src="">
-      <h1 class="title">Tectcone Out Of Context</h1>
-    </div>
-      <div class="quote-container">
-        <p class="quote-text">{{ quoteText }}</p>
-    </div>
-  <button @click="getNewQuote" class="btn-generate">Generate</button>
-  </div>
- </template>
-
-<script setup>
-</script>
-
-
-
-<style scoped>
-</style>
+  <QuoteGenerator />
+</template>
