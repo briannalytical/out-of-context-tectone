600bb318023fb68a9be5011af0e56f839c9beedc
briannalytical <briannamcneil.dev@gmail.com>
2025-08-18 13:01:41 -0400

    appended to add import to app.vue

--- out-of-context-tectone/src/App.vue
+++ out-of-context-tectone/src/App.vue
@@ -1,9 +1,16 @@
+
+
+supabase.from('your_table_name').select('*').then(console.log)
+
 <template>
   <QuoteGenerator />
 </template>
 
 <script>
 import QuoteGenerator from '/Users/breembair/Desktop/Code/out-of-context-tectone/out-of-context-tectone/src/components/QuoteGenerator'
+import { supabase } from '@/supabaseClient'
+supabase.from('your_table_name').select('*').then(console.log)
+
 
 export default {
   name: 'App',
