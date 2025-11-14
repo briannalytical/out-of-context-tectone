7245fe4dcc81df0d9dac330cc61ba63fca14b332
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-11-13 20:24:44 -0500

    redeployment

--- out-of-context-tectone/src/components/QuoteGenerator.vue
+++ out-of-context-tectone/src/components/QuoteGenerator.vue
@@ -55,7 +55,7 @@ const fetchQuote = async () => {
     loading.value = true
 
     const { data: quotes, error } = await supabase
-      .from('public.quotes')
+      .from('quotes')
       .select('id, quote_text')
       .eq('is_used', false)
 
